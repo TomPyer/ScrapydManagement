e8230695f0d3e522d99506e3ddb79d783a934ee6
tangxuelin <jizhidetangxuelin@outlook.com>
2018-01-18 09:43:34 +0800

    新的四大块子链接文件结构(内容编写中)

---
+++ app/databaseMain/__init__.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+from flask import Blueprint
+
+databaseMain = Blueprint('databaseMain', __name__)
+
+from app.databaseMain import views
+
+from . import forms
+# 由于路由和错误处理页面定义在这个文件里面，导入到蓝本把他们关联起来
+# 又因为views.py，error.py需要导入蓝本main，防止循环导入所以放到最后
