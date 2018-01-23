1808525186ab53150f89b6b7bd99470d78401538
tangxuelin <jizhidetangxuelin@outlook.com>
2018-01-23 18:13:27 +0800

    完成task任务创建流程

--- app/taskMain/views.py
+++ app/taskMain/views.py
@@ -3,9 +3,14 @@
 from flask import render_template, request, redirect, url_for, flash
 from app.taskMain import taskMain
 from flask_login import login_required
-from app.models import TaskInfo
+from app.models import TaskInfo, OperaLog
+from app import db
 
 from datetime import datetime
+from utils.SchedUtils import SchedulerUtils
+from app.scdMain.spiderinfo import SpiderTask
+
+import uuid
 
 
 @taskMain.route('/all_task', methods=['GET'])
@@ -14,10 +19,10 @@ def all_task():
     task_info = TaskInfo.query.all()
     re_list = []
     for task in task_info:
-        temp_data = {'task_name': task['name'], 'work_num': task['work_num'], 'create_date': task['create_time'],
-                     'create_pre': task['create_person'], 'status': task['status'], 'introduce': task['introduce']}
+        temp_data = {'task_name': task.name, 'work_num': task.work_num, 'create_date': task.create_time,
+                     'create_pre': task.create_person, 'status': task.status, 'introduce': task.introduce}
         re_list.append(temp_data)
-    return render_template('apsForm/all_task.html', re_list=re_list)
+    return render_template('apsForm/stoped_task.html', re_list=re_list)
 
 
 @taskMain.route('/effe_task', methods=['GET'])
@@ -26,10 +31,10 @@ def effe_task():
     task_info = TaskInfo.query.filter_by(status='running').all()
     re_list = []
     for task in task_info:
-        temp_data = {'task_name': task['name'], 'work_num': task['work_num'], 'create_date': task['create_time'],
-                     'create_pre': task['create_person'], 'status': task['status'], 'introduce': task['introduce']}
+        temp_data = {'task_name': task.name, 'work_num': task.work_num, 'create_date': task.create_time,
+                     'create_pre': task.create_person, 'status': task.status, 'introduce': task.introduce}
         re_list.append(temp_data)
-    return render_template('apsForm/effe_task.html', re_list=re_list)
+    return render_template('apsForm/stoped_task.html', re_list=re_list)
 
 
 @taskMain.route('/stoped_task', methods=['GET'])
@@ -38,16 +43,23 @@ def stoped_task():
     task_info = TaskInfo.query.filter_by(status='stop').all()
     re_list = []
     for task in task_info:
-        temp_data = {'task_name': task['name'], 'work_num': task['work_num'], 'create_date': task['create_time'],
-                     'create_pre': task['create_person'], 'status': task['status'], 'introduce': task['introduce']}
+        temp_data = {'task_name': task.name, 'work_num': task.work_num, 'create_date': task.create_time,
+                     'create_pre': task.create_person, 'status': task.status, 'introduce': task.introduce}
         re_list.append(temp_data)
     return render_template('apsForm/stoped_task.html', re_list=re_list)
+
+
+def myfunc(project, spider):
+    from subprocess import Popen
+    Popen(['scrapy'], cwd='D:/work/scrapyProject/%s' % project ,shell=True)
 
 
 @taskMain.route('/create_task', methods=['POST'])
 def create_task():
     # 创建任务
     try:
+        # sched_obj = SchedulerUtils()
+        stask_obj = SpiderTask()
         # id = db.Column(db.Integer, primary_key=True)
         # name = db.Column(db.String(30), index=True)
         # work_num = db.Column(db.Integer)
@@ -55,14 +67,20 @@ def create_task():
         # create_person = db.Column(db.String(50))
         # status = db.Column(db.String(20))
         # introduce = db.Column(db.String(20))
-        task_info = TaskInfo(name=request.form['name'], work_num=0, create_time=datetime.datetime.now(), conent=request.form['content'],
-                             create_person='tangxl', status='Padding', introduce=request.form['introduce'])
-        # 后面添加读取当前登录用户功能
-        opara_log = OperaLog(operation='create_task', person='txl', operation_name=requesti.form['name'],
-                             create_time=datetime.datetime.now())
-        db.session.add(task_info)
-        db.session.add(opara_log)
-        db.session.commit()
+        # 添加创建定时任务流程
+        # sched_obj.create_task(myfunc(), task_id=task_id, trigger=request.form['dy_type'], **request.form)
+        flag = stask_obj.create_task(request.form['project'], request.form['spider'], 'start', request.form['dy_type'], **request.form)
+        if flag:
+            task_info = TaskInfo(name=request.form['name'], work_num=0, create_time=datetime.now(),
+                                 create_person='tangxl', status='Padding', introduce=request.form['introduce'])
+            # 后面添加读取当前登录用户功能
+            opara_log = OperaLog(operation='create_task', person='txl', operation_name=request.form['name'],
+                                 create_time=datetime.now())
+            db.session.add(task_info)
+            db.session.add(opara_log)
+            db.session.commit()
+        else:
+            flash('创建任务失败...')
     except KeyError as e:
         _ = e
         flash('请填写完整信息...')
